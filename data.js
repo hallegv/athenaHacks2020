209a7ec341df249a3bbfcd9b71cee04ea5ea8821
= <hcomet2062@gmail.com>
2020-03-07 22:13:58 -0800

    preventing merge conflict

---
+++ data.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAula22im3JpO_IG5ILdcpoKzAwXjoUKgE",
+    authDomain: "data-751d3.firebaseapp.com",
+    databaseURL: "https://data-751d3.firebaseio.com",
+    projectId: "data-751d3",
+    storageBucket: "data-751d3.appspot.com",
+    messagingSenderId: "69569862023",
+    appId: "1:69569862023:web:0851c413ac5094f31a5b22",
+    measurementId: "G-Y699CRWFD3"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
